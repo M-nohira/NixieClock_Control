7e3068d5f90fbd4fe2ff6ca7fa0167fcb0735837
野平真義 <csma17068@g.nihon-u.ac.jp>
2019-05-31 01:07:26 +0900

    First Commit

---
+++ NixieClock_Controller/Controller.cpp
@@ -0,0 +1,196 @@
+#include "Controller.h"
+
+int main(void)
+{
+	//Nixie nixie;
+	//nixie.StartProcess();
+	Nixie nixie;
+
+	printf("Starting LogicWoker Process...");
+	nixie.clockTime = clock();
+	std::thread th_logic(&Nixie::LogicWorker, &nixie);
+	while (nixie.logicState == false);
+	printf("Starting DisplayWoker Process...");
+	nixie.clockTime = clock();
+	std::thread th_display(&Nixie::DisplayWorker, &nixie);
+	
+	th_logic.join();
+	th_display.join();
+
+	return 0;
+}
+
+
+
+void Nixie::LogicWorker()
+{
+	
+	do
+	{
+		time_t t_time;
+		time(&t_time);
+		tm tms;
+		tms = *localtime(&t_time);
+		int hh[8] = { tms.tm_hour / 10,tms.tm_hour % 10,tms.tm_min / 10 ,tms.tm_min % 10, tms.tm_sec / 10, tms.tm_sec % 10 };
+		//printf("%d%d:%d%d:%d%d \n\r", tms.tm_hour / 10, tms.tm_hour % 10, tms.tm_min / 10, tms.tm_min % 10, tms.tm_sec / 10, tms.tm_sec % 10);
+		
+		for (int cnt = 0; cnt < 8; cnt++)
+		{
+			DisplaySetting(cnt, cnt + 1, hh[cnt]);
+		}
+
+		if (logicState == false) printf("Started!! in %d\n\r", clock() - clockTime);	//無理やり表示させるために突っ込みました
+
+		logicState = true;
+	} while (true);
+	logicState = false;
+}
+
+void Nixie::DisplayWorker()
+{
+	
+	do
+	{
+		if (true == mutex.try_lock())
+		{
+			memcpy(suborder, order, sizeof(suborder));
+			mutex.unlock();
+		}
+
+		DisplayUpdate();
+
+		if(displayState == false) printf("Started!! in %d\n\r", clock() - clockTime);	//無理やり表示させるために突っ込みました
+
+		displayState = true;
+	} while (true);
+	displayState = false;
+}
+
+/// <summary>
+/// 実行時フラグを設定
+/// </summary>
+/// <param name="flag"></param>
+void Nixie::SetWorkerFlag(bool flag)
+{
+	workerEnableFlag = flag;
+}
+
+void Nixie::SetTube(int bcd1, int bcd2, int bcd3, int bcd4)
+{
+	digitalWrite(12, bcd1);
+	digitalWrite(16, bcd2);
+	digitalWrite(20, bcd3);
+	digitalWrite(21, bcd4);
+}
+
+void Nixie::SetNum(int bcd1, int bcd2, int bcd3, int bcd4)
+{
+	digitalWrite(23, bcd1);
+	digitalWrite(24, bcd2);
+	digitalWrite(25, bcd3);
+	digitalWrite(8, bcd4);
+}
+
+void Nixie::SelectShowing(int num, bool isTube)
+{
+	if (isTube == false)
+	{
+		switch (num)
+		{
+		case 0:
+			SetTube(0, 1, 1, 0);
+		case 1:
+			SetTube(1, 0, 0, 1);
+		case 2:
+			SetTube(1, 0, 0, 0);
+		case 3:
+			SetTube(0, 1, 1, 1);
+		case 4:
+			SetTube(0, 0, 0, 0);
+		case 5:
+			SetTube(0, 0, 0, 1);
+		case 6:
+			SetTube(0, 0, 1, 0);
+		case 7:
+			SetTube(0, 0, 1, 1);
+		case 8:
+			SetTube(0, 1, 0, 0);
+		case 9:
+			SetTube(0, 1, 0, 1);
+		default:
+			SetTube(1, 1, 1, 1);
+		}
+		return;
+	}
+
+	switch (num)
+	{
+	case 0:
+		SetNum(1, 1, 1, 1);
+	case 1:
+		SetNum(0, 0, 1, 1);
+	case 2:
+		SetNum(0, 0, 0, 1);
+	case 3:
+		SetNum(0, 0, 1, 0);
+	case 4:
+		SetNum(0, 0, 0, 0);
+	case 5:
+		SetNum(0, 1, 1, 1);
+	case 6:
+		SetNum(0, 1, 0, 1);
+	case 7:
+		SetNum(0, 1, 1, 0);
+	case 8:
+		SetNum(0, 1, 0, 0);
+	default:
+		SetNum(1, 1, 1, 1);
+	}
+	return;
+}
+
+/// <summary>
+/// 表示用注文行列をもとに表示する.
+/// </summary>
+void Nixie::DisplayUpdate()
+{
+	for (int cnt = 0; cnt < 8; cnt++)
+	{
+		//表示するニキシー管に通電
+		SelectShowing(suborder[0][cnt], true);
+		//表示する文字をGNDの接続      
+		SelectShowing(suborder[1][cnt], false);
+
+		//ゴースト現象回避のために調整する必要アリ
+		delayMicroseconds(NIXIE_ON_TIME);//ON　時間
+
+		//表示を例外を用いて消す
+		SelectShowing(999, true);
+		SelectShowing(999, false);
+
+		delayMicroseconds(NIXIE_OFF_TIME); //OFF 時間
+	}
+}
+
+/// <summary>
+/// 表示用注文行列を編集 要修正
+/// </summary>
+/// <param name="quecount">列数</param>
+/// <param name="tubeNum">表示するニキシー管の番号</param>
+/// <param name="showNum">表示する番号</param>
+void Nixie::DisplaySetting(int quecount, int tubeNum, int showNum)
+{
+	int draft[2][8];
+
+	mutex.lock();
+	memcpy(draft, order, sizeof(draft));
+	mutex.unlock();
+
+	draft[0][quecount] = tubeNum;
+	draft[1][quecount] = showNum;
+
+	mutex.lock();
+	memcpy(order, draft, sizeof(draft));
+	mutex.unlock();
+}
+
---
+++ NixieClock_Controller/Controller.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <stdio.h>
+#include <mutex>
+#include <thread>
+#include <wiringPi.h>
+#include <string.h>
+#include <time.h>
+
+#define NIXIE_ON_TIME   600
+#define NIXIE_OFF_TIME  400
+
+class Nixie
+{
+public:
+	Nixie();
+	~Nixie();
+	
+	bool logicState = false;
+	bool displayState = false;
+	clock_t clockTime; //èàóùéûä‘åvë™ópÅ@ñ≥ë Ç∑Ç¨ÇÈÇØÇ«å„â˜ÇµÇƒÇ»Ç¢
+
+	void DisplayUpdate();
+	void DisplaySetting(int quecount, int tubeNum, int showNum);
+	void LogicWorker();
+	void DisplayWorker();
+	void SetWorkerFlag(bool flag);
+
+private:
+	int order[2][8];
+	int suborder[2][8];
+	std::mutex mutex;
+	bool workerEnableFlag = false;
+
+	
+
+	void SetTube(int bcd1, int bcd2, int bcd3, int bcd4);
+	void SetNum(int bcd1, int bcd2, int bcd3, int bcd4);
+	void SelectShowing(int num,bool isTube);
+};
+
+Nixie::Nixie()
+{
+	printf("Initializing RapberryPi GPIO\n\r");
+	wiringPiSetupGpio();
+	pinMode(12, OUTPUT);
+	pinMode(16, OUTPUT);
+	pinMode(20, OUTPUT);
+	pinMode(21, OUTPUT);
+	pinMode(23, OUTPUT);
+	pinMode(24, OUTPUT);
+	pinMode(25, OUTPUT);
+	pinMode(8, OUTPUT);
+	pinMode(17, OUTPUT);
+	pinMode(27, OUTPUT);
+}
+
+Nixie::~Nixie()
+{
+}
